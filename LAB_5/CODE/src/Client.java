f1232763a7c44bd95b571d3e6e30bc5bea3d3c2f
Sarafudinov <57676105+Sarafudinov@users.noreply.github.com>
2022-04-02 19:38:15 +0300

    Add files via upload

---
+++ LAB_5/CODE/src/Client.java
@@ -0,0 +1,50 @@
+import java.io.*;
+import java.net.Socket;
+import java.util.Locale;
+
+public class Client {
+
+    private static Socket clientSocket; //сокет для общения
+    private static BufferedReader reader; // нам нужен ридер читающий с консоли
+    private static BufferedReader in; // поток чтения из сокета
+    private static BufferedWriter out; // поток записи в сокет
+
+    public static void main(String[] args) {
+        try {
+            try {
+                // адрес - локальный хост, порт - 4004, такой же как у сервера
+                clientSocket = new Socket("localhost", 4004); // этой строкой мы запрашиваем
+                //  у сервера доступ на соединение
+                reader = new BufferedReader(new InputStreamReader(System.in));
+                // читать соообщения с сервера
+                in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                // писать туда же
+                out = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
+
+                while (true) {
+                    System.out.println("Вы что-то хотели сказать? Введите это здесь:");
+                    // если соединение произошло и потоки успешно созданы - мы можем
+                    //  работать дальше и предложить клиенту что то ввести
+                    // если нет - вылетит исключение
+                    String word = reader.readLine(); // ждём пока клиент что-нибудь
+
+                    // не напишет в консоль
+                    out.write(word + "\n"); // отправляем сообщение на сервер
+                    out.flush();
+                    String serverWord = in.readLine(); // ждём, что скажет сервер
+                    System.out.println(serverWord); // получив - выводим на экран
+                    if (word.toLowerCase(Locale.ROOT).equals("exit"))
+                        break;
+                }
+
+            } finally { // в любом случае необходимо закрыть сокет и потоки
+                System.out.println("Клиент был закрыт...");
+                clientSocket.close();
+                in.close();
+                out.close();
+            }
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+        }
+    }
+}
