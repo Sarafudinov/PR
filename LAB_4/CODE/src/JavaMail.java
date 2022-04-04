69ef9079c9ee46b661a87c76eb0b15d22b2f6387
Sarafudinov <57676105+Sarafudinov@users.noreply.github.com>
2022-04-04 21:43:05 +0300

    Update JavaMail.java

--- LAB_4/CODE/src/JavaMail.java
+++ LAB_4/CODE/src/JavaMail.java
@@ -1,6 +1,6 @@
 public class JavaMail {
     public static void main(String[] args) throws Exception {
-            EmailSender.sendMail("testweb.sh@mail.ru");
+            EmailSender.sendMail("RECIPIENT EMAIL");
 
     }
 }
